0b915f1bf339ba5a00d8c54f699da91764f49a27
Dhananjay Deore <dhananjaydeore@Dhananjays-MacBook-Air.local>
2023-06-08 23:10:01 +0530

    emotion styled added

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -49,8 +49,8 @@ const Login = () => {
       const config = {
         headers: {
           "Content-type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          // "Access-Control-Allow-Origin": "*",
+          // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
         },
       };
       axios
